49f88525a8f2ad77ee7ebeb6c83c89e98d24c2ed
spot-jarvis <shefinsharafudheen.s@gmail.com>
2025-11-10 17:53:33 +0530

    Day 3

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+
+
+posts = [
+    {
+        'author' :'Shefin',
+        'title' : 'Life is a dare dream',
+        'content':'Its about you !',
+        'date_post' :'April 20 2025'
+    },
+    {
+        'author' :'Noone',
+        'title' : 'Life lessons',
+        'content':'Its about Me !',
+        'date_post' :'April 30 2025'
+    }
+]
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts,title = 'Home')
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title = 'About ')
 
 if __name__ =="__main__":
     app.run(debug=True)
